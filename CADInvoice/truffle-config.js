68f0c13f5f2c142db8b449e27ba85c5cac66ea9f
Olusegun Komolafe <joshusegun@gmail.com>
2020-03-23 07:53:01 +0100

    Update: Just a few update

--- CADInvoice/truffle-config.js
+++ CADInvoice/truffle-config.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const mnemonic = process.env.MNEMONIC;
 const HDWalletProvider = require("truffle-hdwallet-provider");
 // Create your own key for Production environments (https://infura.io/)
-const INFURA_ID = process.env.INFURA_ID || 'd6760e62b67f4937ba1ea2691046f06d';
+const INFURA_ID = process.env.INFURA_ID || '4c1b13ce2c89403d951d451c575a1f68';
 
 
 const configNetwok = (network, networkId, path = "m/44'/60'/0'/0/", gas = 4465030, gasPrice = 1e10) => ({
   provider: () => new HDWalletProvider(
-    mnemonic, `https://${network}.infura.io/v3/${INFURA_ID}`, 
-        0, 1, true, path
+    mnemonic, `https://ropsten.infura.io/v3/4c1b13ce2c89403d951d451c575a1f68`, 
+        0, 3, true, path
     ),
   networkId,
   gas,
